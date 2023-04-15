11c1e110bdac9df55cc1c8f797089490844c038f
Izuna <fc55179@alunos.fc.ul.pt>
2023-04-15 21:55:43 +0100

    Added query and findAllOpenBills to the BillRepository

--- src/main/java/pt/ul/fc/di/css/alunos/democracia/repositories/BillRepository.java
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/repositories/BillRepository.java
@@ -1,8 +1,16 @@
 package pt.ul.fc.di.css.alunos.democracia.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pt.ul.fc.di.css.alunos.democracia.dataacess.BillStatus;
 import pt.ul.fc.di.css.alunos.democracia.entities.Bill;
 
+import java.util.List;
+
 @Repository
-public interface BillRepository extends JpaRepository<Bill, Long> {}
+public interface BillRepository extends JpaRepository<Bill, Long> {
+    @Query("SELECT b FROM Bill b WHERE b.status = :OPEN")
+    List<Bill> findAllOpenBills();
+}
