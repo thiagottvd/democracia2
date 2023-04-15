6d8e3f08213b70ebe0da0cdcff13aa9a36acd730
Izuna <fc55179@alunos.fc.ul.pt>
2023-04-15 19:02:35 +0100

    Created Citizen class with some JPA annotations.

---
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/entities/Citizen.java
@@ -0,0 +1,16 @@
+package pt.ul.fc.di.css.alunos.democracia.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Citizen {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+}
