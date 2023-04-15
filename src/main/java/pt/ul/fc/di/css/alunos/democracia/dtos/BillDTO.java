e94f7574027b10bf44349fe0564b4130c0b980e3
Izuna <fc55179@alunos.fc.ul.pt>
2023-04-15 21:56:35 +0100

    Updated BillDTO to save the theme as well.

--- src/main/java/pt/ul/fc/di/css/alunos/democracia/dtos/BillDTO.java
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/dtos/BillDTO.java
@@ -2,14 +2,17 @@ package pt.ul.fc.di.css.alunos.democracia.dtos;
 
 import java.time.LocalDate;
 import pt.ul.fc.di.css.alunos.democracia.entities.Bill;
+import pt.ul.fc.di.css.alunos.democracia.entities.Theme;
 
 public class BillDTO {
 
   private String title;
   private LocalDate expirationDate;
+  private Theme theme;
 
   public BillDTO(Bill bill) {
     this.title = bill.getTitle();
     this.expirationDate = bill.getExpirationDate();
+    this.theme = bill.getTheme();
   }
 }
