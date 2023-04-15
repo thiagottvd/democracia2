377eef20bd1d2d2805d8715d6390919adeff2733
Izuna <fc55179@alunos.fc.ul.pt>
2023-04-15 18:19:36 +0100

    Added all necessary attributes to the class and updated the constructor accordingly

--- src/main/java/pt/ul/fc/di/css/alunos/democracia/entities/Bill.java
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/entities/Bill.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import pt.ul.fc.di.css.alunos.democracia.dataacess.BillStatus;
+import pt.ul.fc.di.css.alunos.democracia.entities.Delegate;
+
+import java.io.File;
 import java.time.LocalDate;
 
 @Entity
@@ -11,16 +15,29 @@ public class Bill {
 
   @Id @GeneratedValue private Long id;
   private String title;
+  private String description;
+  private File file;
   private LocalDate expirationDate;
+  private BillStatus status;
+  private int numSupporters;
+
+  @OneToOne
+  private Theme theme;
+  @OneToOne
+  private Delegate delegate;
   @OneToOne private Poll associatedPoll;
 
   protected Bill() {
     // Empty constructor required by JPA.
   }
 
-  public Bill(String title, LocalDate expirationDate) {
+  public Bill(String title, String description, File file, LocalDate expirationDate, Delegate delegate, Theme theme) {
     this.title = title;
+    this.description = description;
+    this.file = file;
     this.expirationDate = expirationDate;
+    this.delegate = delegate;
+    this.theme = theme;
   }
 
   public Long getId() {
