dd22d98cc802e859e69e081936394e44b8dcc4db
fc53636 <fc53636@alunos.ciencias.ulisboa.pt>
2023-04-18 07:21:23 +0100

    Adicionada a documentação para todas as classes relativas ao caso de uso F (menos a classe de testes).

--- src/main/java/pt/ul/fc/di/css/alunos/democracia/handlers/CloseExpiredBillsHandler.java
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/handlers/CloseExpiredBillsHandler.java
@@ -4,16 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.ul.fc.di.css.alunos.democracia.catalogs.BillCatalog;
 
+/**
+ * Use case F.
+ *
+ * <p>Handler that closes all expired bills.
+ */
 @Component
 public class CloseExpiredBillsHandler {
 
   private final BillCatalog billCatalog;
 
+  /**
+   * Constructor for the CloseExpiredBillsHandler class. It takes a BillCatalog object as parameter
+   * and sets it as an attribute.
+   *
+   * @param billCatalog the catalog responsible for managing bills.
+   */
   @Autowired
   public CloseExpiredBillsHandler(BillCatalog billCatalog) {
     this.billCatalog = billCatalog;
   }
 
+  /** Closes all bills that have expired. */
   public void closeExpiredBills() {
     billCatalog.closeExpiredBills();
   }
