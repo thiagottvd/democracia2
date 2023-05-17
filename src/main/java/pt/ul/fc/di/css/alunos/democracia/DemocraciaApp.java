91286c96db969cff83c28c43bfb91a802155f5a4
fc53636 <fc53636@alunos.ciencias.ulisboa.pt>
2023-05-17 17:35:01 +0100

    Renomeada classe com método main de 'Application.java' para 'DemocraciaApp.java' e renomeada classe de testes de 'ApplicationTests.java' para 'DemocraciaAppTests.java'.

---
+++ src/main/java/pt/ul/fc/di/css/alunos/democracia/DemocraciaApp.java
@@ -0,0 +1,23 @@
+package pt.ul.fc.di.css.alunos.democracia;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemocraciaApp {
+
+  private static final Logger log = LoggerFactory.getLogger(DemocraciaApp.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemocraciaApp.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner demo() {
+    return (args) -> {};
+  }
+}
