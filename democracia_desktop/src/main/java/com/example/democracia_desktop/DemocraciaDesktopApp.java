2a5898a19526ca7f43efb6f7a522ddf7833261a7
fc53636 <fc53636@alunos.ciencias.ulisboa.pt>
2023-05-29 23:41:17 +0100

    Final commit, adios.

--- democracia_desktop/src/main/java/com/example/democracia_desktop/DemocraciaDesktopApp.java
+++ democracia_desktop/src/main/java/com/example/democracia_desktop/DemocraciaDesktopApp.java
@@ -1,23 +1,23 @@
 package com.example.democracia_desktop;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class DemocraciaDesktopApp extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DemocraciaDesktopApp.class.getResource("authentication.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Democracia 2.0");
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader =
+        new FXMLLoader(DemocraciaDesktopApp.class.getResource("authentication.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+    stage.setTitle("Democracia 2.0");
+    stage.setScene(scene);
+    stage.show();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 }
--- democracia_desktop/src/main/java/module-info.java
+++ democracia_desktop/src/main/java/module-info.java
@@ -1,18 +1,21 @@
 module com.example.democracia_desktop {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
-    requires java.net.http;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.datatype.jsr310;
-    requires javafx.web;
-    requires java.desktop;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires org.controlsfx.controls;
+  requires org.kordamp.bootstrapfx.core;
+  requires java.net.http;
+  requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.datatype.jsr310;
+  requires javafx.web;
+  requires java.desktop;
 
-    opens com.example.democracia_desktop to javafx.fxml;
-    exports com.example.democracia_desktop;
-    exports com.example.democracia_desktop.controllers;
-    exports com.example.democracia_desktop.models;
-    opens com.example.democracia_desktop.controllers to javafx.fxml;
+  opens com.example.democracia_desktop to
+      javafx.fxml;
+
+  exports com.example.democracia_desktop;
+  exports com.example.democracia_desktop.controllers;
+  exports com.example.democracia_desktop.models;
+
+  opens com.example.democracia_desktop.controllers to
+      javafx.fxml;
 }
